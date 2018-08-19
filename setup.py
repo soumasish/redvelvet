18a23acfb71995447da2d96b5e28ddcc054905fe
sgoswami <sgoswami@oath.com>
2018-08-19 12:28:26 -0700

    Updated long description.

--- setup.py
+++ setup.py
@@ -1,9 +1,14 @@
 from distutils.core import setup
 
+with open('README.md') as file:
+    long_description = file.read()
+
+
 setup(
     name = 'redvelvet',
-    version = '0.0.2',
+    version = '0.0.3',
     description = 'A package for some common data types',
+    long_description=long_description,
     author = 'soumasish',
     author_email = 'soumasish@gmail.com',
     url = 'https://github.com/soumasish/redvelvet',
