c1f1ff219e7079d969c8237d50b8013a5cb6b295
sgoswami <sgoswami@oath.com>
2018-08-19 12:08:18 -0700

    Updated packages and bumped version settings.

--- setup.py
+++ setup.py
@@ -2,12 +2,12 @@ from distutils.core import setup
 
 setup(
     name = 'redvelvet',
-    version = '0.0.1',
+    version = '0.0.2',
     description = 'A package for some common data types',
     author = 'soumasish',
     author_email = 'soumasish@gmail.com',
     url = 'https://github.com/soumasish/redvelvet',
     license = 'Apache 2.0',
-    packages = ['graph', 'heap', 'trie'],
+    packages = ['redvelvet.graph', 'redvelvet.heap', 'redvelvet.trie'],
     entry_points = {},
 )
